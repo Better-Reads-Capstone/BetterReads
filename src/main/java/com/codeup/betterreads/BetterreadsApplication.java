8f0c7914aa080b84fc9dd5ae8e0f3422503c1c41
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-12-14 09:18:52 -0600

    feat: starting better reads project.

---
+++ src/main/java/com/codeup/betterreads/BetterreadsApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.betterreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterreadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetterreadsApplication.class, args);
+    }
+
+}
