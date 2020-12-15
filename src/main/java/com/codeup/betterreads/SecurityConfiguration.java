4f7c0daab869b5dfda76a584ec4a1b33ff561de8
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-12-15 14:48:18 -0600

    feat: create UserController to properly create a new User and allow the user to create a profile

--- src/main/java/com/codeup/betterreads/SecurityConfiguration.java
+++ src/main/java/com/codeup/betterreads/SecurityConfiguration.java
@@ -1,35 +1,35 @@
 package com.codeup.betterreads;
 
-//import com.codeup.betterreads.services.UserDetailsLoader;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
+import com.codeup.betterreads.services.UserDetailsLoader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-//@Configuration
-//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-//
-//    private UserDetailsLoader usersLoader;
-//
-//    public SecurityConfiguration(UserDetailsLoader usersLoader) {
-//        this.usersLoader = usersLoader;
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .userDetailsService(usersLoader) // How to find users by their username
-//                .passwordEncoder(passwordEncoder()) // How to encode and verify passwords
-//        ;
-//    }
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private UserDetailsLoader usersLoader;
+
+    public SecurityConfiguration(UserDetailsLoader usersLoader) {
+        this.usersLoader = usersLoader;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(usersLoader) // How to find users by their username
+                .passwordEncoder(passwordEncoder()) // How to encode and verify passwords
+        ;
+    }
 
 //    @Override
 //    protected void configure(HttpSecurity http) throws Exception {
@@ -57,4 +57,4 @@ package com.codeup.betterreads;
 //                .authenticated()
 //        ;
 //    }
-//}
+}
