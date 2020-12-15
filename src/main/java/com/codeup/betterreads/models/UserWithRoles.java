3f4179b435189815997e0cbfe65cc666c3c6d882
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-12-15 13:36:10 -0600

    feat: create controller for index page

--- src/main/java/com/codeup/betterreads/models/UserWithRoles.java
+++ src/main/java/com/codeup/betterreads/models/UserWithRoles.java
@@ -6,35 +6,35 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
-public class UserWithRoles extends User implements UserDetails {
-
-    public UserWithRoles(User user) {
-        super(user);  // Call the copy constructor defined in User
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        String roles = ""; // Since we're not using the authorization part of the component
-        return AuthorityUtils.commaSeparatedStringToAuthorityList(roles);
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-}
+//public class UserWithRoles extends User implements UserDetails {
+//
+//    public UserWithRoles(User user) {
+//        super(user);  // Call the copy constructor defined in User
+//    }
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        String roles = ""; // Since we're not using the authorization part of the component
+//        return AuthorityUtils.commaSeparatedStringToAuthorityList(roles);
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return true;
+//    }
+//}
