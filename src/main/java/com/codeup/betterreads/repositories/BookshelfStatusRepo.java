7640d18076afbc720d50638a598749faed65830b
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-12-15 12:42:43 -0600

    feat: completed the foundation templates from login, register, navbar, and included logout button in the navbar.

--- src/main/java/com/codeup/betterreads/repositories/BookshelfStatusRepo.java
+++
@@ -1,7 +0,0 @@
-package com.codeup.betterreads.repositories;
-
-import com.codeup.betterreads.models.BookshelfStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookshelfStatusRepo extends JpaRepository<BookshelfStatus, Long> {
-}
