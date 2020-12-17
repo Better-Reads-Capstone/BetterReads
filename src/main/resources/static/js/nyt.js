611d64848d843f657a6686663d83e978dd6ccac5
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-12-17 15:19:57 -0600

    feat: stores isbns to local storage.

--- src/main/resources/static/js/nyt.js
+++ src/main/resources/static/js/nyt.js
@@ -1,21 +1,64 @@
-const url = "https://api.nytimes.com/svc/books/v3/lists.json?list="
-const apiKey = `&api-key=${nytAPIKey}`
+const url = "https://api.nytimes.com/svc/books/v3/lists.json?list=";
+const apiKey = `&api-key=${nytAPIKey}`;
 const urlList = ["combined-print-and-e-book-fiction","hardcover-fiction","trade-fiction-paperback","combined-print-and-e-book-nonfiction","hardcover-nonfiction","paperback-nonfiction", "advice-how-to-and-miscellaneous"];
-const selectors = ["print-ebook-fiction-list","hardcover-fiction-list", "trade-fiction-paperback-list", "print-ebook-nonfiction-list", "hardcover-nonfiction-list", "paperback-nonfiction-list", "advice-how-to-list"]
+const selectors = ["print-ebook-fiction-list","hardcover-fiction-list", "trade-fiction-paperback-list", "print-ebook-nonfiction-list", "hardcover-nonfiction-list", "paperback-nonfiction-list", "advice-how-to-list"];
 
 
-function displayBestSellers() {
+
+function checkLocalStorage() {
+    if(window.localStorage.length === 0) {
+        saveBestSellersISBNS();
+    }
+    let retrievedISBNs = localStorage.getItem("ISBNS");
+    let storedISBNs = JSON.parse(retrievedISBNs);
+    console.log(storedISBNs);
+
+
+
+}
+
+checkLocalStorage();
+
+//Using Local Storage to save the best sellers ISBNs
+function saveBestSellersISBNS() {
+    let ISBNs = [];
+
     for(let i = 0; i < urlList.length; i++) {
         fetch(`${url}${urlList[i]}${apiKey}`)
             .then(response => response.json())
             .then(data => {
-                document.querySelector(`.${selectors[i]}`).innerHTML = displayBookCard(data);
+                for(let x = 0; x < data.results.length; x++) {
+                    let isbn = data.results[x].isbns[0].isbn13;
+                    // let title = data.results[x].book_details[0].title
+                    ISBNs.push(isbn);
+                }
+
+                localStorage.setItem("ISBNS", JSON.stringify(ISBNs));
             })
             .catch(errors => console.log(errors))
     }
 }
 
-displayBestSellers();
+
+$(".clear-local").click(function (event) {
+    event.preventDefault();
+    window.localStorage.clear();
+})
+
+
+
+// function displayBestSellers() {
+//     for(let i = 0; i < urlList.length; i++) {
+//         fetch(`${url}${urlList[i]}${apiKey}`)
+//             .then(response => response.json())
+//             .then(data => {
+//                 document.querySelector(`.${selectors[i]}`).innerHTML = displayBookCard(data);
+//             })
+//             .catch(errors => console.log(errors))
+//     }
+// }
+
+// displayBestSellers();
 
 function displayBookCard(data) {
     let finalHTML = '';
