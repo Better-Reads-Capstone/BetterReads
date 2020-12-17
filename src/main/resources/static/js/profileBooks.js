7ae4f19d1398663cb009c95b82f43929e9abe85d
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-12-17 16:13:10 -0600

    feat: create js to properly extract book info and store the passed information into a book object, that gets passed into an array of books to be used in template creation on profile page.

--- src/main/resources/static/js/profileBooks.js
+++ src/main/resources/static/js/profileBooks.js
@@ -4,25 +4,46 @@ function generatePath(path) {
     return getUrl;
 }
 
+//creates an array of isbns to be passed into fetch call
+let isbns = [];
 $('.userBook').each(function () {
-    let isbn = $(this).attr('value')
-    //now that I have access I can come up with a get api call to
-    //retrieve book information and create a template to be inserted into html
-
-    const getBooks = () => fetch(generatePath(isbn))
-        .then(res => res.json())
-        .then(books => {
-            console.log(books)})
-
-
-    // document.onreadystatechange = function () {
-    //     if (document.readyState === "complete") {
-    //         $('#loader').remove()
-    //         getBooks()
-    //     }
-    // };
-    getBooks()
+    isbns.push($(this).attr('value'))
 })
+
+
+let books = [];
+for(let isbn of isbns) {
+    const getBook = async () => fetch(generatePath(isbn))
+        .then(res => res.json())
+        .then(book => {
+            //WILL DRILL TO BASE ITEMS
+            // console.log(book.items[0])
+            let drillPath = book.items[0].volumeInfo;
+            // console.log(drillPath);
+            console.log(book.items[0].saleInfo.buyLink);
+
+            let createBook = {};
+            createBook.img = drillPath.imageLinks.smallThumbnail;
+            createBook.title = drillPath.title;
+            createBook.authors = drillPath.authors;
+            createBook.description = drillPath.description;
+            createBook.publishedDate = drillPath.publishedDate;
+            createBook.language = drillPath.language;
+            createBook.categories = drillPath.categories;
+            createBook.saleInfo = book.items[0].saleInfo.buyLink;
+            return createBook;
+        })
+
+    const setBook = async () => {
+        let extractedBook = await getBook();
+        books.push(extractedBook)
+
+    }
+    setBook()
+}
+console.log(books);
+
+
 
 
 
