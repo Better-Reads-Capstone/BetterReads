e600c8a063e69a15373fd6e5f94e380da4dc7799
Michael Troia <troia.michael.a@gmail.com>
2020-12-17 11:49:34 -0600

    removed privacy, fixed null genre return

--- src/main/java/com/codeup/betterreads/controllers/ClubController.java
+++ src/main/java/com/codeup/betterreads/controllers/ClubController.java
@@ -1,6 +1,7 @@
 package com.codeup.betterreads.controllers;
 
 import com.codeup.betterreads.models.Club;
+import com.codeup.betterreads.models.Genre;
 import com.codeup.betterreads.models.User;
 import com.codeup.betterreads.repositories.ClubRepo;
 import com.codeup.betterreads.repositories.UserRepo;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.persistence.GeneratedValue;
 import java.util.Date;
 
 @Controller
@@ -38,6 +40,7 @@ public class ClubController {
                              @ModelAttribute Club club) {
         User user = userDao.findByUsername(username);
         Date currentDate = new Date();
+
         club.setOwner(user);
         club.setCreatedDate(currentDate);
 
