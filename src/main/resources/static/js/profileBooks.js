1e759276249c01c85c1734d28a52d6d9d1dfc909
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-12-16 19:09:56 -0600

    feat: create /js/profileBooks.js and have a function that will get the stored isbn values in a users bookshelf

---
+++ src/main/resources/static/js/profileBooks.js
@@ -0,0 +1,9 @@
+//will access the stored value of each element with the class userBook
+const getUrl = 
+
+$('.userBook').each(function () {
+    $(this).attr('value')
+    //now that I have access I can come up with a get api call to
+    //retrieve book information and create a template to be inserted into html
+
+})
