338652049b8aab8f4c848dc59b7d41461c2bb657
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-12-17 21:44:15 -0600

    feat: refactor profile-page to pass isbn to the elements id, also got a solution using setTimeout to get each book from books array

--- src/main/resources/static/js/profileBooks.js
+++ src/main/resources/static/js/profileBooks.js
@@ -7,43 +7,88 @@ function generatePath(path) {
 //creates an array of isbns to be passed into fetch call
 let isbns = [];
 $('.userBook').each(function () {
-    isbns.push($(this).attr('value'))
+    isbns.push($(this).attr('id'))
 })
+
+//gets and builds book objects and stores them into a books array
+
+
+// async function myFetch() {
+//     for (let isbn of isbns) {
+//         let response = await fetch(generatePath(isbn));
+//         let extractBook = response.json();
+//         let drillPath = book.items[0].volumeInfo;
+//         let book = {};
+//
+//         if (!response.ok) {
+//             throw new Error(`HTTP error! status: ${response.status}`);
+//         } else {
+//             let book = await response;
+//             console.log(book)
+//         }
+//     }
+// }
+//
+// myFetch()
+//     .catch(e => {
+//         console.log('There has been a problem with your fetch operation: ' + e.message);
+//     });
 
 
 let books = [];
-for(let isbn of isbns) {
-    const getBook = async () => fetch(generatePath(isbn))
-        .then(res => res.json())
-        .then(book => {
-            //WILL DRILL TO BASE ITEMS
-            // console.log(book.items[0])
-            let drillPath = book.items[0].volumeInfo;
-            // console.log(drillPath);
-            console.log(book.items[0].saleInfo.buyLink);
+const getBook = () => {
+    for (let isbn of isbns) {
+        fetch(generatePath(isbn))
+            .then(res => res.json())
+            .then(book => {
 
-            let createBook = {};
-            createBook.img = drillPath.imageLinks.smallThumbnail;
-            createBook.title = drillPath.title;
-            createBook.authors = drillPath.authors;
-            createBook.description = drillPath.description;
-            createBook.publishedDate = drillPath.publishedDate;
-            createBook.language = drillPath.language;
-            createBook.categories = drillPath.categories;
-            createBook.saleInfo = book.items[0].saleInfo.buyLink;
-            return createBook;
-        })
+                //WILL DRILL TO BASE ITEMS
+                let drillPath = book.items[0].volumeInfo;
 
-    const setBook = async () => {
-        let extractedBook = await getBook();
-        books.push(extractedBook)
-
+                let createBook = {};
+                createBook.img = drillPath.imageLinks.smallThumbnail;
+                createBook.title = drillPath.title;
+                createBook.authors = drillPath.authors;
+                createBook.description = drillPath.description;
+                createBook.publishedDate = drillPath.publishedDate;
+                createBook.language = drillPath.language;
+                createBook.categories = drillPath.categories;
+                createBook.isbn = drillPath.industryIdentifiers;
+                createBook.saleInfo = book.items[0].saleInfo.buyLink;
+                // return createBook;
+                books.push(createBook)
+            })
+            .catch(error => console.error("ERROR"))
     }
-    setBook()
 }
-console.log(books);
+getBook()
+//
+// const setBook = async () => {
+//     const book = await getBook()
+//     return book
+// }
+// setBook().then(book => {
+//     console.log(book)
+// })
 
 
+// //will display each book after the timeout
+//
+setTimeout(() =>
+{
+
+for(let book of books) {
+
+    console.log(book)
+}
+        // voila!
+
+}, 1000);
+
+// console.log(books)
+// $('.userBook').each(function() {
+//     console.log($(this).attr('value'));
+// })
 
 
 
