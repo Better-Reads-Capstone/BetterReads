016cc2ab23ef7cce6100741bbfea4f7696d041d5
Levi Trejo <levi.b.trejo@gmail.com>
2020-12-18 17:53:29 -0600

    NOTE: testing branch, do not commit

--- src/main/java/com/codeup/betterreads/controllers/BookController.java
+++ src/main/java/com/codeup/betterreads/controllers/BookController.java
@@ -2,16 +2,24 @@ package com.codeup.betterreads.controllers;
 
 import com.codeup.betterreads.models.Book;
 import com.codeup.betterreads.repositories.BookRepo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class BookController {
 
     private BookRepo bookDao;
+
+    @Value("${googleBooksAPIKey}")
+    private String bgapikey;
+
+    @Value("${nytAPIKey}")
+    private String nytapikey;
+
+    @Value("${filestackAPIKey}")
+    private String filestackkey;
 
     public BookController(BookRepo bookDao) {
         this.bookDao = bookDao;
@@ -43,5 +51,19 @@ public class BookController {
         Book dbBook = bookDao.findBookByIsbnTenEquals(isbn);
         viewModel.addAttribute("book", dbBook);
         return "books/viewbook";
+    }
+
+    // Creates a psuedo keys.js file
+    // TODO: Need to lockdown access to this path to internal requests only (CORS/CSRF/Security settings)
+    @RequestMapping(path = "/keys.js", produces = "application/javascript")
+    @ResponseBody
+    public String apikeys(){
+        String filedata = String.format("const gbKey='%s';%nconst nytKey='%s';%nconst fsKey='%s'", bgapikey, nytapikey, filestackkey);
+        return filedata;
+    }
+
+    @GetMapping("/books")
+    public String showBooks(){
+        return "books/playground";
     }
 }
--- src/main/resources/static/JS/books_api.js
+++ src/main/resources/static/JS/books_api.js
@@ -1,38 +1,161 @@
-const baseUrl = new URL('https://www.googleapis.com/books/v1/volumes?');
-const apiKey = booksApiKey;
+const baseUrl = new URL('https://www.googleapis.com/books/v1/volumes');
+const apiKey = gbKey;
 
 function simpleQuery(searchTerm) {
-    fetch(baseUrl + new URLSearchParams({
+    document.getElementById("search-results").innerHTML = "";
+    fetch(baseUrl + '?' + new URLSearchParams({
         q: searchTerm,
+        printType: 'books',
         key: apiKey
     }))
         .then(response => response.json())
         .then(result => {
-            for (let i = 0; i < result.items.length; i++) {
-                let item = result.items[i];
-                console.log(item);
-                document.getElementById("search-results").innerHTML += "<div class='card mb-3' style='max-width: 540px;'>";
-                document.getElementById("search-results").innerHTML += "<div class='row no-gutters'>";
-                document.getElementById("search-results").innerHTML += "<div class='col-md-4'>";
-                document.getElementById("search-results").innerHTML += `<img src='${item.volumeInfo.imageLinks.thumbnail}' class='card-img' alt='thumbnail-image' style='width: 70px'>`;
-                document.getElementById("search-results").innerHTML += "</div>";
-                document.getElementById("search-results").innerHTML += "<div class='col-md-8'>";
-                document.getElementById("search-results").innerHTML += "<div class='card-body'>";
-                document.getElementById("search-results").innerHTML += `<h5 class="card-title>">${item.volumeInfo.title}</h5>`;
-                document.getElementById("search-results").innerHTML += `<p class='card-text'>${item.volumeInfo.description}</p>`;
-                document.getElementById("search-results").innerHTML += `<p class='card-text'>${item.volumeInfo.industryIdentifiers[0].identifier}</p>`;
-                document.getElementById("search-results").innerHTML += `<form class='form-inline my-2 my-lg-0' method="POST" action="/book/${item.volumeInfo.industryIdentifiers[0].identifier}"><button class='btn my-2 my-sm-0' type='submit' id='addBookBtn'>Add Book</button>`;
-                document.getElementById("search-results").innerHTML += "</form>";
-                document.getElementById("search-results").innerHTML += "</div>";
-                document.getElementById("search-results").innerHTML += "</div>";
-                document.getElementById("search-results").innerHTML += "</div>";
-                document.getElementById("search-results").innerHTML += "</div>";
-            }
+            let finalhtml = displayMultiBookCards(result);
+            // let finalhtml = buildIsbns(result);
+            document.getElementById("search-results").innerHTML += finalhtml;
+            })
+        .catch(e => {
+            console.log(e);
         })
+}
+
+function isbnQuery(isbn) {
+    document.getElementById("search-results").innerHTML = "";
+    fetch(baseUrl + '?' + new URLSearchParams({
+        q: 'isbn:' + isbn,
+        printType: 'books',
+        key: apiKey
+    }))
+        .then(response => response.json())
+        .then(result => {
+            let finalhtml = displayMultiBookCards(result);
+            document.getElementById("search-results").innerHTML += finalhtml;
+        })
+        .catch(e => {
+            console.log(e);
+        })
+}
+
+function referenceQuery(gbid) {
+    document.getElementById("search-results").innerHTML = "";
+    fetch(baseUrl + '/' + gbid)
+        .then(response => response.json())
+        .then(result => {
+            let finalhtml = displaySingleBookCard(result);
+            document.getElementById("search-results").innerHTML += finalhtml;
+        })
+        .catch(e => {
+            console.log(e);
+        })
+}
+
+function buildIsbns(data){
+    console.log(data)
+    let isbn = [];
+    for (let i = 0; i < data.items.length; i++){
+        let isbnObject = {};
+        let book = data.items[i];
+        for (let x = 0; x < book.volumeInfo.industryIdentifiers.length; x++) {
+            isbnObject.type = book.volumeInfo.industryIdentifiers[x].type;
+            isbnObject.isbn = book.volumeInfo.industryIdentifiers[x].identifier;
+            isbn.push(isbnObject)
+            console.log(isbnObject);
+        }
+    }
+    return isbn;
+}
+
+function displayMultiBookCards(data) {
+    console.log(data);
+    let finalHTML = '';
+    for(let i = 0; i < data.items.length; i++) {
+        let book = data.items[i];
+
+        // set a default thumbnail, overwrite if possible
+        let thumbnail = 'https://via.placeholder.com/75';
+
+        // This is needed in case a book resource doesn't have a thumbnail
+        try {
+            thumbnail = book.volumeInfo.imageLinks.thumbnail;
+        } catch (e) {
+            console.log("api thumbnail is undefined");
+        }
+
+        finalHTML +=
+            `<div class="col mb-4">
+                        <div class="card">
+                            <div class="card-header">
+                                <p>${book.volumeInfo.title}</p>
+                            </div>
+                            <div class="card-body">
+                                <img src='${thumbnail}' class='card-img' alt='thumbnail-image' style='width: 70px'>
+                                <p>${book.volumeInfo.industryIdentifiers[0].identifier}</p>
+                            </div>
+                            <div class="card-footer">
+                                <form>
+                                    <input hidden value="${book.volumeInfo.industryIdentifiers[0].identifier}">
+                                    <button type="submit" class="btn btn-primary">Add Book!</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>`
+    }
+
+    return finalHTML;
+}
+
+function getGoogleRef(isbn){
+    // make an API query that takes an ISBN number and returns a Google Books reference ID
+}
+
+function displaySingleBookCard(data) {
+    console.log(data);
+    let finalHTML;
+    let book = data;
+
+    // set a default thumbnail, overwrite if possible
+    let thumbnail = 'https://via.placeholder.com/75';
+    try {
+        thumbnail = book.volumeInfo.imageLinks.thumbnail;
+    } catch (e) {
+        console.log("api thumbnail is undefined");
+    }
+
+    finalHTML =
+        `<div class="col mb-4">
+                    <div class="card">
+                        <div class="card-header">
+                            <p>${book.volumeInfo.title}</p>
+                        </div>
+                        <div class="card-body">
+                            <img src='${thumbnail}' class='card-img' alt='thumbnail-image' style='width: 70px'>
+                            <p>${book.volumeInfo.industryIdentifiers[0].identifier}</p>
+                        </div>
+                        <div class="card-footer">
+                            <form>
+                                <input hidden value="${book.volumeInfo.industryIdentifiers[0].identifier}">
+                                <button type="submit" class="btn btn-primary">Add Book!</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>`
+    return finalHTML;
 }
 
 $('#searchbtn').click(function(event) {
     event.preventDefault();
     let searchterm = $('#searchterm').val();
     simpleQuery(searchterm);
+})
+
+$('#isbntermbtn').click(function(event) {
+    event.preventDefault();
+    let isbn = $('#isbnterm').val();
+    isbnQuery(isbn)
+})
+
+$('#reftermbtn').click(function(event) {
+    event.preventDefault();
+    let reference = $('#refterm').val();
+    referenceQuery(reference)
 })
