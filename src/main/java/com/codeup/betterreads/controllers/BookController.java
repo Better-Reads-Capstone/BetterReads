f712f2432fa4da3ca873df3fd315884bb78e8c12
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-12-30 15:00:12 -0600

    feat: write html to viewbook.html that will display a form to allow a user to write a review, see all reviews, or edit and delete a review if they own the review. Will have jquery to place the values of the current review to populate the review modal. Will also provide the proper controller to allow a user to post a new review or edit a review.

--- src/main/java/com/codeup/betterreads/controllers/BookController.java
+++ src/main/java/com/codeup/betterreads/controllers/BookController.java
@@ -1,20 +1,29 @@
 package com.codeup.betterreads.controllers;
 
 import com.codeup.betterreads.models.Book;
+import com.codeup.betterreads.models.Review;
+import com.codeup.betterreads.models.User;
 import com.codeup.betterreads.repositories.BookRepo;
+import com.codeup.betterreads.repositories.ReviewRepo;
+import com.codeup.betterreads.repositories.UserRepo;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class BookController {
 
     private BookRepo bookDao;
+    private UserRepo userDao;
+    private ReviewRepo reviewDao;
 
-    public BookController(BookRepo bookDao) {
+    public BookController(BookRepo bookDao, UserRepo userDao, ReviewRepo reviewDao) {
         this.bookDao = bookDao;
+        this.userDao = userDao;
+        this.reviewDao = reviewDao;
     }
 
     @GetMapping("/booksearch")
@@ -29,9 +38,47 @@ public class BookController {
             newDbBook.setGbreference(gbreference);
             newBook = bookDao.save(newDbBook);
         }
-
+        //Should get all reviews by the created books id
+        List<Review> dbReviews = reviewDao.findAllByBookId(newBook.getId());
+        //places a new review object to review form and places all reviews retrieved to view
+        viewModel.addAttribute("review", new Review());
+        viewModel.addAttribute("reviews", dbReviews);
         viewModel.addAttribute("book", newBook);
         return "books/viewbook";
+    }
+
+    //will need to test these two PostMappings After Levi completes viewbook
+    @PostMapping("/book/{bookId}/createReview/{username}")
+    public String createReview(@PathVariable long bookId,
+                               @PathVariable String username,
+                               @ModelAttribute Review dbReview
+    ){
+        User dbUser = userDao.findByUsername(username);
+        Book dbBook = bookDao.getOne(bookId);
+        dbReview.setOwner(dbUser);
+        dbReview.setBook(dbBook);
+        reviewDao.save(dbReview);
+        //may need to change to redirect to the viewedBook
+        return "redirect:/books/viewbook";
+    }
+
+    @PostMapping("/{username}/{book}/editReview/{reviewId}")
+    public String editReview(@PathVariable String username,
+                             @PathVariable Book book,
+                             @PathVariable long reviewId,
+                             @ModelAttribute Review reviewToBeEdited
+    ){
+        User dbUser = userDao.findByUsername(username);
+        Review extractedReview = reviewDao.getOne(reviewId);
+
+        reviewToBeEdited.setOwner(dbUser);
+        reviewToBeEdited.setBook(book);
+        reviewToBeEdited.setId(reviewId);
+        reviewToBeEdited.setCreatedDate(extractedReview.getCreatedDate());
+        reviewDao.save(reviewToBeEdited);
+
+        //may need to change to redirect to the viewedBook
+        return "redirect:/books/viewbook";
     }
 
 //    @GetMapping("/book/{isbn}")
--- src/main/java/com/codeup/betterreads/repositories/ReviewRepo.java
+++ src/main/java/com/codeup/betterreads/repositories/ReviewRepo.java
@@ -3,6 +3,9 @@ package com.codeup.betterreads.repositories;
 import com.codeup.betterreads.models.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepo extends JpaRepository<Review, Long> {
     Review findByOwnerIdAndBookId(long ownerId, long bookId);
+    List<Review> findAllByBookId(long bookId);
 }
