7278074662f5ff7eb547e7c30f5a3c677d2fdad6
Levi Trejo <levi.b.trejo@gmail.com>
2021-01-03 22:11:51 -0600

    feat: Mail service added

---
+++ src/main/java/com/codeup/betterreads/services/MailService.java
@@ -0,0 +1,35 @@
+package com.codeup.betterreads.services;
+
+import com.codeup.betterreads.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service("mailService")
+public class MailService {
+
+    @Autowired
+    public JavaMailSender mailsender;
+
+    @Value("${spring.mail.from}")
+    public String from;
+
+    public void prepareAndSend(User user, String subject, String body) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setFrom(from);
+        msg.setTo(user.getEmail());
+        msg.setSubject(subject);
+        msg.setText(body);
+
+        try {
+            this.mailsender.send(msg);
+        }
+        catch (MailException ex){
+            System.err.println(ex.getMessage());
+        }
+    }
+
+}
