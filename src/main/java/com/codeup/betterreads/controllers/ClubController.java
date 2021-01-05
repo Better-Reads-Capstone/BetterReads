65b7fbc773fcfb4060e4ebfed08e56c9aec842b1
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2021-01-04 23:17:13 -0600

    feat: a club admin or owner can now create a blog post and edit the post. Using CKEditor 5 for the blog body.

--- src/main/java/com/codeup/betterreads/controllers/ClubController.java
+++ src/main/java/com/codeup/betterreads/controllers/ClubController.java
@@ -1,11 +1,9 @@
 package com.codeup.betterreads.controllers;
 
-import com.codeup.betterreads.models.Club;
-import com.codeup.betterreads.models.ClubMember;
-import com.codeup.betterreads.models.Genre;
-import com.codeup.betterreads.models.User;
+import com.codeup.betterreads.models.*;
 import com.codeup.betterreads.repositories.ClubMemberRepo;
 import com.codeup.betterreads.repositories.ClubRepo;
+import com.codeup.betterreads.repositories.PostRepo;
 import com.codeup.betterreads.repositories.UserRepo;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,11 +23,13 @@ public class ClubController {
     private UserRepo userDao;
     private ClubRepo clubDao;
     private ClubMemberRepo clubMemberDao;
+    private PostRepo postDao;
 
-    public ClubController(UserRepo userDao, ClubRepo clubDao, ClubMemberRepo clubMemberDao) {
+    public ClubController(UserRepo userDao, ClubRepo clubDao, ClubMemberRepo clubMemberDao, PostRepo postDao) {
         this.userDao = userDao;
         this.clubDao = clubDao;
         this.clubMemberDao = clubMemberDao;
+        this.postDao = postDao;
     }
 
     // Create Club
@@ -128,4 +128,73 @@ public class ClubController {
         return "redirect:/bookclub/" + dbClub.getId();
     }
 
+    //Create Post
+    @GetMapping("/bookclub/{id}/create-post")
+    public String viewCreatePostForm(Model viewModel, @PathVariable long id) {
+        viewModel.addAttribute("post", new Post());
+        viewModel.addAttribute("club", clubDao.getOne(id));
+        return "user/create-post";
+    }
+
+    @PostMapping("/bookclub/{id}/create-post")
+    public String createPost(
+            @PathVariable long id,
+            @ModelAttribute Club club,
+            @ModelAttribute Post post) {
+
+        Date currentDate = new Date();
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        post.setClub(club);
+        post.setUser(user);
+        post.setCreatedDate(currentDate);
+        post.setUpdatedDate(currentDate);
+
+        Post dbPost = postDao.save(post);
+
+        return "redirect:/bookclub/" + id + "/" + dbPost.getId();
+    }
+
+    @GetMapping("/bookclub/{id}/{postId}")
+    public String viewPost(
+            Model viewModel,
+            @PathVariable long id,
+            @PathVariable long postId) {
+
+        viewModel.addAttribute("post", postDao.getOne(postId));
+        viewModel.addAttribute("club", clubDao.getOne(id));
+
+        return "user/club-post";
+    }
+
+    @GetMapping("/bookclub/{id}/edit-post/{postId}")
+    public String viewEditPostForm(
+            Model viewModel,
+            @PathVariable long id,
+            @PathVariable long postId) {
+
+        viewModel.addAttribute("post", postDao.getOne(postId));
+        viewModel.addAttribute("club", clubDao.getOne(id));
+
+        return "user/create-post";
+    }
+
+    @PostMapping("/bookclub/{id}/edit-post/{postId}")
+    public String editPost(
+            @ModelAttribute Post post,
+            @ModelAttribute Club club,
+            @PathVariable long id,
+            @PathVariable long postId) {
+
+        Post postToBeUpdated = postDao.getOne(postId);
+        Date currentDate = new Date();
+
+        postToBeUpdated.setUpdatedDate(currentDate);
+        postToBeUpdated.setCreatedDate(post.getCreatedDate());
+        postToBeUpdated.setUser(post.getUser());
+        postToBeUpdated.setClub(post.getClub());
+
+        Post dbPost = postDao.save(postToBeUpdated);
+        return "redirect:/bookclub/" + id + "/" + dbPost.getId();
+    }
 }
