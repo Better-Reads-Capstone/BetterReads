7fcb2e7e145724768d7a1004d02ecf0d486a85bf
Levi Trejo <levi.b.trejo@gmail.com>
2021-01-05 22:39:55 -0600

    WIP: debugging spring EL and date formatting

--- src/main/resources/static/js/utils/datepicker.js
+++ src/main/resources/static/js/utils/datepicker.js
@@ -1,3 +1,14 @@
-let picker = new Pikaday(
-    { field: document.getElementById('dob')}
-)
+let picker = new Pikaday({
+    field: document.getElementById('dob'),
+    yearRange: [1900, 2030],
+    format: 'YYYY-MM-DD',
+    toString(date, format) {
+        const day = date.getDate();
+        let month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        if (month < 10) {
+            month = '0' + month.toString();
+        }
+        return `${year}-${month}-${day}`;
+    }
+    })
