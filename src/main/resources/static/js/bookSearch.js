9f72c04fb809f8276dd5488b85b25312421552b8
Levi Trejo <levi.b.trejo@gmail.com>
2021-01-09 21:11:36 -0600

    feat: refactored search functional, viewbook refactor functional

--- src/main/resources/static/js/bookSearch.js
+++ src/main/resources/static/js/bookSearch.js
@@ -1,28 +1,19 @@
 $(document).ready(function(){
-    let searchvalue = null;
-    try {
-        searchvalue = $("#result-term").attr("value");
-    } catch (err) {}
+    let searchvalue = $("#result-term").attr("value") ?? null;
 
     if (searchvalue != null) {
-        // Deploy loading icon
         showLoadingIcon();
-
-        // Empty HTML element
         let results = $('#search-results');
         results.empty();
-
-        // Fetch data, format data, populate
-        fetchQuery(searchvalue).then(data => {
-            let booksArr = (createBookObjects(data));
-            let finalHTML = '';
-            for (const book of booksArr) {
-                finalHTML += buildSmallBookCard(book);
-            }
-            results.append(finalHTML);
-
-            // remove the loading icon
-            hideLoadingIcon();
+        queryFetch(searchvalue)
+            .then(data => {
+                let booksArr = (createBookObjects(data));
+                let finalHTML = '';
+                for (const book of booksArr) {
+                    finalHTML += buildSmallBookCard(book);
+                }
+                results.append(finalHTML);
+                hideLoadingIcon();
         })
             .catch(err => {
                 console.log(err)
--- src/main/resources/static/js/books_api.js
+++ src/main/resources/static/js/books_api.js
@@ -1,25 +1,7 @@
 const baseUrl = new URL('https://www.googleapis.com/books/v1/volumes');
 const apiKey = googleBooksAPIKey;
 
-// Fetch functions
-const simpleQuery = (searchTerm, elementId, callback) => {
-    document.getElementById(elementId).innerHTML = "";
-    fetch(baseUrl + '?' + new URLSearchParams({
-        q: searchTerm,
-        printType: 'books',
-        key: apiKey
-    }))
-        .then(response => response.json())
-        .then(result => {
-            let finalhtml = callback(result);
-            document.getElementById(elementId).innerHTML += finalhtml;
-        })
-        .catch(e => {
-            console.log(e);
-        })
-}
-
-const fetchQuery = (searchTerm) => {
+const queryFetch = (searchTerm) => {
     return new Promise((resolve) => {
         fetch(baseUrl + '?' + new URLSearchParams({
             q: searchTerm,
@@ -34,23 +16,6 @@ const fetchQuery = (searchTerm) => {
     })
 }
 
-const isbnQuery = (isbn, elementId, callback) => {
-    document.getElementById(elementId).innerHTML = "";
-    fetch(baseUrl + '?' + new URLSearchParams({
-        q: 'isbn:' + isbn,
-        printType: 'books',
-        key: apiKey
-    }))
-        .then(response => response.json())
-        .then(result => {
-            let finalhtml = callback(result);
-            document.getElementById(elementId).innerHTML += finalhtml;
-        })
-        .catch(e => {
-            console.log(e);
-        })
-}
-
 const isbnFetch = (isbn) => {
     return new Promise((resolve) => {
         fetch(baseUrl + '?' + new URLSearchParams({
@@ -63,6 +28,14 @@ const isbnFetch = (isbn) => {
     })
 }
 
+const referenceFetch = (gbid) => {
+    return new Promise((resolve) => {
+        fetch(baseUrl + '/' + gbid)
+            .then(response => response.json())
+            .then(data => {resolve(data)});
+        })
+}
+
 const titleAuthorFetch = (title, author) => {
     return fetch(baseUrl + '?' + new URLSearchParams({
         q: title + '+inauthor' + author,
@@ -71,81 +44,6 @@ const titleAuthorFetch = (title, author) => {
     }))
         .then(response => response.json())
 
-}
-
-const referenceQuery = (gbid, elementId, callback) => {
-    document.getElementById(elementId).innerHTML = "";
-    fetch(baseUrl + '/' + gbid)
-        .then(response => response.json())
-        .then(result => {
-            let finalhtml = callback(result);
-            document.getElementById(elementId).innerHTML += finalhtml;
-        })
-        .catch(e => {
-            console.log(e);
-        })
-}
-
-// Callback functions to manipulate the data
-
-const displayMultiBookCards = (data) => {
-    let finalHTML = '';
-    for(let i = 0; i < data.items.length; i++) {
-        let book = data.items[i];
-        let thumbnail = 'https://via.placeholder.com/75';
-        let isbn = "Unable to search for book"
-        try {
-            thumbnail = book.volumeInfo.imageLinks.thumbnail;
-        } catch (error) {
-            console.error("No thumbnail exists for this book.")
-        }
-        try {
-            isbn = book.volumeInfo.industryIdentifiers[0].identifier
-        } catch (error) {
-            console.log("Unable to search for book")
-        }
-        finalHTML +=
-            `<div class="col mb-4">
-                        <div class="card">
-                            <div class="card-header">
-                                <p>${book.volumeInfo.title}</p>
-                            </div>
-                            <div class="card-body">
-                                <img src='${thumbnail}' class='card-img' alt='thumbnail-image' style='width: 70px'>
-                                <p>ISBN: ${isbn}</p>
-                            </div>
-                            <div class="card-footer">
-                                <form class='form-inline my-2 my-lg-0' method="GET" action="/book/${book.id}"><button class='btn btn-success my-2 my-sm-0' type='submit' id='viewBookBtn'>View Book</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>`
-    }
-
-    return finalHTML;
-}
-
-const displaySingleBookCard = (data) => {
-    let finalHTML;
-    let book = data;
-    let thumbnail = 'https://via.placeholder.com/75';
-    try {
-        thumbnail = book.volumeInfo.imageLinks.thumbnail;
-    } catch (error) {
-        console.error("No thumbnail exists for this book.")
-    }
-    finalHTML =
-        `<div class="card-header">
-             <p>${book.volumeInfo.title}</p>
-         </div>
-         <div class="card-body">
-             <img src='${thumbnail}' class='card-img' alt='thumbnail-image' style='width: 70px'>
-             <p>ISBN: ${book.volumeInfo.industryIdentifiers[0].identifier}</p>
-             <p>Author: ${book.volumeInfo.authors[0]}</p>
-             <p>${book.volumeInfo.description}</p>
-             <p>Book ID: ${book.id}</p>
-         </div>`
-    return finalHTML;
 }
 
 const createBookObjects = (data) => {
@@ -185,6 +83,38 @@ const createBookObjects = (data) => {
     return formattedBooks;
 }
 
+const createSingleBookObj = (data) => {
+    const bookAPIObj = {
+        title: data.volumeInfo.title,
+        auth: data.volumeInfo.authors ?? 'Data unavailable',
+        id: data.id,
+        desc: data.volumeInfo.description ?? 'Unavailable',
+        descSm: data.volumeInfo.description ?? 'Unavailable',
+        pubDate: data.volumeInfo.publishedDate,
+        publisher: data.volumeInfo.publisher,
+        img: data.volumeInfo.imageLinks ?? '/img/logo.png',
+        isbn: data.volumeInfo.industryIdentifiers ?? [{type: 'ISBN', value: 'Unavailable'}]
+    }
+
+    if (bookAPIObj.auth !== 'Data unavailable') {
+        bookAPIObj.auth = data.volumeInfo.authors[0];
+    }
+
+    if (bookAPIObj.img !== '/img/logo.png'){
+        if (typeof data.volumeInfo.imageLinks.thumbnail !== 'undefined') {
+            bookAPIObj.img = data.volumeInfo.imageLinks.thumbnail;
+        } else if (typeof data.volumeInfo.imageLinks.smallThumbnail !== 'undefined') {
+            bookAPIObj.img = data.volumeInfo.imageLinks.smallThumbnail;
+        }
+    }
+
+    if(bookAPIObj.desc.length > 100) {
+        bookAPIObj.descSm = bookAPIObj.desc.slice(0, 100) + '...'
+    }
+
+    return bookAPIObj;
+}
+
 const buildSmallBookCard = (book) => {
     let cardHTML = '';
     cardHTML =
@@ -194,7 +124,7 @@ const buildSmallBookCard = (book) => {
                             <div class="card-body">
                                 <h5 class="text-center">${book.title}</h5>
                                 <p>Author: ${book.auth}</p>
-                                <p>${book.isbn[0].type}: ${book.isbn[0].identifier}</p>
+                                <p>ISBN: ${book.isbn[0].identifier}</p>
                                 <p>Publisher: ${book.publisher}</p>
                                 <p>Date Published: ${book.pubDate}</p>
                                 <p>Description: ${book.descSm}</p>
@@ -206,4 +136,21 @@ const buildSmallBookCard = (book) => {
                         </div>
                     </div>`
     return cardHTML;
+}
+
+const buildLargeSingleCard = (book) => {
+    let cardHTML = '';
+    cardHTML =
+        `<div class="card-header">
+             <p>${book.title}</p>
+         </div>
+         <div class="card-body">
+             <img src='${book.img}' class='card-img' alt="Book cover for ${book.title}" style='width: 18rem'>
+             <p>ISBN: ${book.isbn[0].identifier}</p>
+             <p>Author: ${book.auth}</p>
+             <p>Description:</p>
+             <p>${book.desc}</p>
+         </div>`
+    return cardHTML;
+
 }
